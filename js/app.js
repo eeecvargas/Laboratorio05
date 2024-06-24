1e97632b376c7696f4438f09784f1e840be39bfe
eeecvargas <eecvargas.pe@gmail.com>
2024-06-24 16:48:47 -0500

    Implementación de la función

--- js/app.js
+++ js/app.js
@@ -70,12 +70,22 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    const sum = sumArr.reduce(sum, 0);
 
+    // Convert the array elements to a string with commas and spaces
+    const stringArr = sumArr.join(", ");
+  
+    // Create the message string using string concatenation
+    const message = `${stringArr} was passed in as an array of numbers, and ${sum} is their sum.`;
+  
+    // Return an array with the sum and the message string
+    return [sum, message];
 }
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
